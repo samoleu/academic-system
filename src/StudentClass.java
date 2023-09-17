6282da077cd596aff1e1b25b78b06b5b5a031ad5
samoleu <samuel.ferralves@gmail.com>
2023-09-16 21:04:25 -0300

    add StudentClass and Professor

---
+++ src/StudentClass.java
@@ -0,0 +1,11 @@
+public class StudentClass {
+    private String name;
+    private int year;
+    private int Semester;
+    private Professor professor;
+    private Student[] students;
+    private Assessment[] assessments;
+
+
+
+}
